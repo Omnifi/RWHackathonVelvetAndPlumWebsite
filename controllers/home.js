92fda5bbb8a506ff1847406ee79912195bca5522
Simon Donaldson <simondonaldson@gmail.com>
2016-09-14 19:21:12 +0100

    fix

--- controllers/home.js
+++ controllers/home.js
@@ -9,19 +9,19 @@ exports.index = (req, res) => {
 
 
 
-    const prod = new Product({
-        name: 'Shirt'
-    });
+    // const prod = new Product({
+    //     name: 'Shirt'
+    // });
 
 
-    prod.save((err) => {
-        if (err) {
-            return next(err);
-        }
+    // prod.save((err) => {
+    //     if (err) {
+    //         return next(err);
+    //     }
         res.render('home', {
             title: 'Home'
         });
-    });
+    // });
 
 
 
