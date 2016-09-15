e30943eb482f986427968efb4aaf7ab365e9ae30
Simon Donaldson <simondonaldson@gmail.com>
2016-09-15 11:09:18 +0100

    ping service with new product

--- controllers/product.js
+++ controllers/product.js
@@ -31,6 +31,19 @@ exports.index = (req, res, next) => {
 exports.add = (req, res, next) => {
     if (!req.params.product_id) return next(new Error('No product id'));
     if (!req.session.basket) req.session.basket = [];
-    if (req.session.basket.indexOf(req.params.product_id) < 0) req.session.basket.push(req.params.product_id);
+    if (req.session.basket.indexOf(req.params.product_id) < 0) {
+        req.session.basket.push(req.params.product_id);
+        Product.find({ _id: req.params.product_id }).exec(function(err, product) {
+            if (product[0] && req.user) {
+                var form = {
+                    'user_id': String(req.user._id),
+                    'name': product[0].name,
+                    'nsfw': product[0].nsfw,
+                    'trend': product[0].trend
+                }
+                request.post({ url: 'https://rwhackathonbasketcase.herokuapp.com/api/product/add', form: form })
+            }
+        });
+    }
     res.redirect('/product/' + req.params.product_id);
 };
